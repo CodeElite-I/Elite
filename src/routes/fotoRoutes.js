9dd44df2fcf7dc2ff44b5c37bab5567eeb2aabdf
boldrini <luizboldriniej@gmail.com>
2024-03-18 21:30:57 -0300

    feat: Implementado rota post com token

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import fotoController from '../controllers/FotoController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
-router.post('/', fotoController.create);
+router.post('/', loginRequired, fotoController.create);
 
 export default router;
