432aee0920e0f8467945a15335995f74aebbe9c6
boldrini <luizboldriniej@gmail.com>
2024-03-18 21:27:49 -0300

    fix: implementado token em rota post

--- src/routes/servicoRoutes.js
+++ src/routes/servicoRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ServicoController from '../controllers/ServicoController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = Router();
 
 router
-  .post('/', ServicoController.create)
+  .post('/', loginRequired, ServicoController.create)
   .get('/', ServicoController.findAll)
   .get('/:id', ServicoController.findById)
   .put('/:id', ServicoController.update)
