6eacb3395ece04f27844166a0ebbdd4ee1855284
boldrini <luizboldriniej@gmail.com>
2024-03-20 22:08:40 -0300

    feat: criando arquivos estáticos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
@@ -40,6 +41,7 @@ class App {
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
   routes() {
