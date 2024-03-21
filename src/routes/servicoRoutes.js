87de9a4f04e24edf8fe5d31a3133dbe55ef463ad
boldrini <luizboldriniej@gmail.com>
2024-03-20 22:09:12 -0300

    fix: Privando toda rota de serviços

--- src/routes/servicoRoutes.js
+++ src/routes/servicoRoutes.js
@@ -7,9 +7,9 @@ const router = Router();
 
 router
   .post('/', loginRequired, ServicoController.create)
-  .get('/', ServicoController.findAll)
-  .get('/:id', ServicoController.findById)
-  .put('/:id', ServicoController.update)
-  .delete('/:id', ServicoController.delete);
+  .get('/', loginRequired, ServicoController.findAll)
+  .get('/:id', loginRequired, ServicoController.findById)
+  .put('/:id', loginRequired, ServicoController.update)
+  .delete('/:id', loginRequired, ServicoController.delete);
 
 export default router;
